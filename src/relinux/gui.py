9be41832533c7a93eb688e1cfc886609d0472c44
Anonymous Meerkat <meerkatanonymous@gmail.com>
2012-06-20 22:03:57 -0600

    Removed the annoying comments on the scrolling frame

--- src/relinux/gui.py
+++ src/relinux/gui.py
@@ -18,30 +18,21 @@ class VerticalScrolledFrame(tkinter.Frame):
                         yscrollcommand=vscrollbar.set)
         canvas.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=tkinter.TRUE)
         vscrollbar.config(command=canvas.yview)
-
-        # reset the view
         canvas.xview_moveto(0)
         canvas.yview_moveto(0)
-
-        # create a frame inside the canvas which will be scrolled with it
         self.interior = interior = tkinter.Frame(canvas)
         interior_id = canvas.create_window(0, 0, window=interior,
                                            anchor=tkinter.NW)
 
-        # track changes to the canvas and frame width and sync them,
-        # also updating the scrollbar
         def _configure_interior(event):
-            # update the scrollbars to match the size of the inner frame
             size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
             canvas.config(scrollregion="0 0 %s %s" % size)
             if interior.winfo_reqwidth() != canvas.winfo_width():
-                # update the canvas's width to fit the inner frame
                 canvas.config(width=interior.winfo_reqwidth())
         interior.bind('<Configure>', _configure_interior)
 
         def _configure_canvas(event):
             if interior.winfo_reqwidth() != canvas.winfo_width():
-                # update the inner frame's width to fill the canvas
                 canvas.itemconfigure(interior_id, width=canvas.winfo_width())
         canvas.bind('<Configure>', _configure_canvas)
 
