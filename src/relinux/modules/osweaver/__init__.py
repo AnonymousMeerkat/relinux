6964dc8869841da88e3fcd5a201f5ad58c6d6ebd
Anonymous Meerkat <meerkatanonymous@gmail.com>
2012-08-16 15:26:05 -0600

    Test

--- src/relinux/modules/osweaver/__init__.py
+++ src/relinux/modules/osweaver/__init__.py
@@ -30,6 +30,7 @@ def runThreads():
 def run(adict):
     global configs, aptcache, page, isotreel, tmpsys
     configs = adict["config"]["OSWeaver"]
+    print(configs)
     isodir = configutils.getValue(configs[configutils.isodir])
     config.ISOTree = isodir + "/.ISO_STRUCTURE/"
     print(config.ISOTree)
