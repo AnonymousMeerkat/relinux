b03a552a03c39fb64a917be6f5df94dfb854a403
Anonymous Meerkat <meerkatanonymous@gmail.com>
2012-08-16 16:31:17 -0600

    fixed test

--- src/relinux/threadmanager.py
+++ src/relinux/threadmanager.py
@@ -35,7 +35,7 @@ def runThread(threadid, threadsdone, threadsrunning, threads):
     thread = getThread(threadid, threads)
     if not thread["thread"].isAlive() and not threadid in threadsdone:
         threadsrunning.append(threadid)
-        print(threadid)
+        print(str(threadid) + " " + str(threadsdone))
         thread["thread"].start()
 
 
