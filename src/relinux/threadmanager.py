de411b551c053f4f1e757e0a9fa590bae32b9979
Anonymous Meerkat <meerkatanonymous@gmail.com>
2012-08-16 14:29:05 -0600

    New system for the thread manager

--- src/relinux/threadmanager.py
+++ src/relinux/threadmanager.py
@@ -10,16 +10,17 @@ import time
 
 
 # Finds threads that can currently run (and have not already run)
-def findRunnableThreads(threads, threadsdone, threadsrunning):
+def findRunnableThreads(threadids, threadsdone, threadsrunning, threads):
     returnme = []
     cpumax = fsutil.getCPUCount()
     current = 0
-    for i in threads:
-        if not i["tn"] in threadsdone and current < cpumax:
+    for i in threadids:
+        if not i in threadsdone and current < cpumax:
+            thread = getThread(i, threads)
             deps = 0
-            depsl = len(i["deps"])
-            for x in i["deps"]:
-                if x["tn"] in threadsdone:
+            depsl = len(thread["deps"])
+            for x in thread["deps"]:
+                if x in threadsdone:
                     deps = deps + 1
             if deps >= depsl:
                 returnme.append(i)
@@ -30,29 +31,46 @@ def findRunnableThreads(threads, threadsdone, threadsrunning):
 
 
 # Run a thread
-def runThread(thread, threadsdone, threadsrunning):
-    if not thread["thread"].isAlive() and not thread["tn"] in threadsdone:
-        threadsrunning.append(thread["tn"])
-        thread["thread"].start()
+def runThread(threadid, threadsdone, threadsrunning, threads):
+    thread = getThread(threadid, threads)
+    if not thread.isAlive() and not threadid in threadsdone:
+        threadsrunning.append(threadid)
+        thread.start()
 
 
 # Check if a thread is alive
-def checkThread(thread, threadsdone, threadsrunning):
-    if thread["tn"] in threadsrunning:
-        if not thread["thread"].isAlive():
-            threadsrunning.remove(thread["tn"])
-            threadsdone.append(thread["tn"])
+def checkThread(threadid, threadsdone, threadsrunning, threads):
+    if threadid in threadsrunning:
+        if not getThread(threadid, threads)["thread"].isAlive():
+            threadsrunning.remove(threadid)
+            threadsdone.append(threadid)
+
+
+# Get a thread from an ID
+def getThread(threadid, threads):
+    return threads[threadid]
 
 
 # Thread loop
 def threadLoop(threads):
     threadsdone = []
     threadsrunning = []
+    threadids = []
+    for i in range(len(threads)):
+        threadids[i] = i
+    for i in range(len(threads)):
+        for x in range(len(threads[i]["deps"])):
+            if threads[i]["deps"][x] in threads:
+                val = 0
+                for y in range(len(threads)):
+                    if threads[i]["deps"][x] == threads[y]:
+                        val = y
+                threads[i]["deps"][x] = val
     while config.ThreadStop is False:
         # Clear old threads
         for x in threadsrunning:
-            checkThread(x, threadsdone, threadsrunning)
+            checkThread(x, threadsdone, threadsrunning, threads)
         # Run runnable threads
-        for x in findRunnableThreads(threads, threadsdone, threadsrunning):
-            runThread(x, threadsdone, threadsrunning)
+        for x in findRunnableThreads(threadids, threadsdone, threadsrunning, threads):
+            runThread(x, threadsdone, threadsrunning, threads)
         time.sleep(float(1.0 / config.ThreadRPS))
