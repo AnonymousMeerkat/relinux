74b0492b3805cbf5574639bd8a1bf119440a67bf
Anonymous Meerkat <meerkatanonymous@gmail.com>
2012-10-14 13:08:15 -0600

    Just a test to see the problem

--- src/relinux/configutils.py
+++ src/relinux/configutils.py
@@ -167,6 +167,7 @@ def getProperties(buffers):
 
 # Returns the value for an property (it will only show the first result, so you have to run getLinesWithinOption)
 def getProperty(buffers, option):
+    print(str(type(option)) + " " + str(option))
     patt = re.compile("^ *" + option + " *:(.*)")
     for i in buffers:
         m = patt.match(i)
